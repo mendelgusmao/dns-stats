36824d0d2f0d420ef4b90c0f6c5746252d25af5e
MendelGusmao <mendelsongusmao@gmail.com>
2013-10-31 09:22:21 -0200

    Rewriting Stats

--- stats/stats.go
+++ stats/stats.go
@@ -5,151 +5,173 @@ import (
 	"fmt"
 	"net/http"
 	"sort"
-	"strconv"
 	"strings"
 	"time"
 )
+
+type fetcher func(*sqlite3.Conn, string) ([]string, int)
 
 var (
 	statsPort = ":8514"
 	format    = "02/01/06 15:04:05"
 	lines     = 25
 
-	sql = `SELECT DISTINCT fqdn AS origin
+	sql = `SELECT DISTINCT fqdn
 		   FROM hosts, queries
 		   WHERE id = origin`
-	sql1 = `SELECT fqdn, COUNT(*) AS c 
-			FROM hosts, queries
-			WHERE origin IN (SELECT id FROM hosts WHERE fqdn LIKE $origin)
-			AND id = destination
-			GROUP BY fqdn
-			ORDER BY c DESC
-			LIMIT 25`
-	sql2 = `SELECT date, fqdn
-			FROM hosts, queries
-			WHERE origin IN (SELECT id FROM hosts WHERE fqdn LIKE $origin)
-			AND id = destination
-			ORDER BY date DESC
-			LIMIT 25`
+	sqlTop = `SELECT fqdn, COUNT(*) AS c 
+			  FROM hosts, queries
+			  WHERE origin IN (SELECT id FROM hosts WHERE fqdn LIKE $origin)
+			  AND id = destination
+			  GROUP BY fqdn
+			  ORDER BY c DESC
+			  LIMIT 25`
+	sqlRecent = `SELECT date, fqdn
+				 FROM hosts, queries
+				 WHERE origin IN (SELECT id FROM hosts WHERE fqdn LIKE $origin)
+				 AND id = destination
+				 ORDER BY date DESC
+				 LIMIT 25`
+
+	fetchers = []fetcher{fetchTopQueries, fetchRecentQueries}
+	net      = "192.168.0.%"
 )
 
 func Stats(dbname string) {
 	fmt.Println("Initializing HTTP stats")
 
 	http.HandleFunc("/dns", func(w http.ResponseWriter, r *http.Request) {
-		var err error
-		var db *sqlite3.Conn
-		if db, err = sqlite3.Open(dbname); err != nil {
-			fmt.Println("Error opening database:", err)
-			return
-		}
-
-		start := time.Now()
-
-		buffer := make([]string, lines*2+4)
-		origins := make([]string, 0)
-
-		for s, err := db.Query(sql); err == nil; err = s.Next() {
-			row := make(sqlite3.RowMap)
-			errs := s.Scan(row)
-
-			if errs != nil {
-				fmt.Println("Error scanning:", errs)
-				return
-			}
-
-			origins = append(origins, row["origin"].(string))
-		}
-
-		neworigins := []string{"192.168.0.%"}
-		sort.Sort(vector(origins))
-		neworigins = append(neworigins, origins...)
-		origins = neworigins
-
-		for _, origin := range origins {
-			i := 0
-			prebuffer := make([]string, lines*2+4)
-			prebuffer[i] = strings.Replace(origin, "%", "0", -1)
-			i++
-			prebuffer[i] = ""
-			i++
-			max := len(origin)
-
-			for s2, err := db.Query(sql1, origin); err == nil; err = s2.Next() {
-				row := make(sqlite3.RowMap)
-				errs := s2.Scan(row)
-
-				if errs != nil {
-					fmt.Println("Error scanning:", errs)
-					return
-				}
-
-				count := row["c"].(int64)
-				line := fmt.Sprintf("%-6d %s", count, row["fqdn"])
-				if len(line) > max {
-					max = len(line)
-				}
-				prebuffer[i] = line
-				i++
-			}
-
-			i = lines + 1
-			prebuffer[i] = ""
-			i++
-
-			for s1, err := db.Query(sql2, origin); err == nil; err = s1.Next() {
-				row := make(sqlite3.RowMap)
-				errs := s1.Scan(row)
-
-				if errs != nil {
-					fmt.Println("Error scanning:", errs)
-					return
-				}
-
-				line := fmt.Sprintf("%s %s", row["date"].(time.Time).Format(format), row["fqdn"])
-				if len(line) > max {
-					max = len(line)
-				}
-				prebuffer[i] = line
-				i++
-			}
-
-			for index, line := range prebuffer {
-				buffer[index] = fmt.Sprintf(fmt.Sprintf("%%s%%-%ds", max+5), buffer[index], line)
-			}
-		}
-
-		buffer[len(buffer)-1] = fmt.Sprintf("took %f seconds to generate", time.Now().Sub(start).Seconds())
-
 		w.Header().Add("Content-Type", "text/plain")
-		fmt.Fprintln(w, strings.Join(buffer, "\n"))
-
-		if err := db.Close(); err != nil {
-			fmt.Println("Error closing database:", err)
-		}
+		fmt.Fprintln(w, fetchStats(dbname))
 	})
 
 	fmt.Println(http.ListenAndServe(statsPort, nil))
 }
 
-type vector []string
+func fetchStats(dbname string) string {
+	var err error
+	var db *sqlite3.Conn
+	if db, err = sqlite3.Open(dbname); err != nil {
+		fmt.Println("Error opening database:", err)
+		return ""
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			fmt.Println("Error closing database:", err)
+		}
+	}()
 
-func (v vector) Len() int {
-	return len(v)
+	buffersLength := (lines * len(fetchers)) + 5
+	start := time.Now()
+	buffer := make([]string, buffersLength)
+	origins := fetchOrigins(db)
+
+	for _, origin := range origins {
+		prebuffer := make([]string, buffersLength)
+		fmt.Println("PreBuffer size is", len(prebuffer))
+		prebuffer[0] = strings.Replace(origin, "%", "0", -1)
+		max := len(prebuffer[0])
+		i := 2
+
+		for _, fetcher := range fetchers {
+			queries, newMax := fetcher(db, origin)
+
+			for _, query := range queries {
+				prebuffer[i] = query
+
+				if newMax > max {
+					max = newMax
+				}
+
+				i++
+			}
+
+			prebuffer[i] = ""
+			i++
+		}
+
+		for index, line := range prebuffer {
+			buffer[index] = fmt.Sprintf(fmt.Sprintf("%%s%%-%ds", max+5), buffer[index], line)
+		}
+	}
+
+	buffer[len(buffer)-1] = fmt.Sprintf("took %f seconds to generate", time.Now().Sub(start).Seconds())
+
+	return strings.Join(buffer, "\n")
 }
 
-func (v vector) Swap(i, j int) {
-	v[i], v[j] = v[j], v[i]
+func fetchOrigins(db *sqlite3.Conn) []string {
+	origins := make([]string, 1)
+
+	for stmt, err := db.Query(sql); err == nil; err = stmt.Next() {
+		row := make(sqlite3.RowMap)
+		errs := stmt.Scan(row)
+
+		if errs != nil {
+			fmt.Println("Error scanning:", errs)
+			return nil
+		}
+
+		origins = append(origins, row["fqdn"].(string))
+	}
+
+	sort.Sort(vector(origins))
+	origins[0] = net
+
+	return origins
 }
 
-// default comparison.
-func (v vector) Less(i, j int) bool {
-	return v.value(v[i]) > v.value(v[j])
+func fetchTopQueries(db *sqlite3.Conn, origin string) ([]string, int) {
+	queries := make([]string, lines)
+	max := 0
+	index := 0
+
+	for stmt, err := db.Query(sqlTop, origin); err == nil; err = stmt.Next() {
+		row := make(sqlite3.RowMap)
+		errs := stmt.Scan(row)
+
+		if errs != nil {
+			fmt.Println("Error scanning:", errs)
+			return queries, 0
+		}
+
+		count := row["c"].(int64)
+		line := fmt.Sprintf("%-6d %s", count, row["fqdn"])
+
+		if len(line) > max {
+			max = len(line)
+		}
+
+		queries[index] = line
+		index++
+	}
+
+	return queries, max
 }
 
-func (v vector) value(in string) (out int) {
-	blocks := strings.Split(in, ".")
-	block := blocks[len(blocks)-1]
-	out, _ = strconv.Atoi(block)
-	return
+func fetchRecentQueries(db *sqlite3.Conn, origin string) ([]string, int) {
+	queries := make([]string, lines)
+	max := 0
+	index := 0
+
+	for stmt, err := db.Query(sqlRecent, origin); err == nil; err = stmt.Next() {
+		row := make(sqlite3.RowMap)
+		errs := stmt.Scan(row)
+
+		if errs != nil {
+			fmt.Println("Error scanning:", errs)
+			return queries, 0
+		}
+
+		line := fmt.Sprintf("%s %s", row["date"].(time.Time).Format(format), row["fqdn"])
+
+		if len(line) > max {
+			max = len(line)
+		}
+
+		queries[index] = line
+		index++
+	}
+
+	return queries, max
 }
